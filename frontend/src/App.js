e404276a7d40d8d42e379e6915ea538d05bf8cd6
tran vinh <tranvinhlx135@gmail.com>
2022-04-24 15:47:11 +0700

    done lesson 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Movie from "./components/movie";
+import MoviesList from "./components/movies-list";
+import AddReview from "./components/add-review";
+import Login from "./components/login";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>
+            <Nav.Link>
+              {user ? (
+                <a href="/" onClick={logout}>
+                  Logout
+                </a>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Switch>
+        <Route exact path={["/", "movies"]} component={MoviesList}></Route>
+        <Route
+          path="/movies/:id/review"
+          render={(props) => <AddReview {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/movies/:id/"
+          render={(props) => <Movie {...props} user={user} />}
+        ></Route>
+        <Route
+          path="/login"
+          render={(props) => <Login {...props} login={login} />}
+        ></Route>
+      </Switch>
     </div>
   );
 }
---
+++ frontend/src/components/movies-list.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MoviesList() {
+  return <div className="App">Movies List - 19522527</div>;
+}
+
+export default MoviesList;
